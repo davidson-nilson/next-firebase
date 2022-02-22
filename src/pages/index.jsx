d44f912641a3c99a125680d03a1553450cb00af7
Davidson <70776280+davidson-nilson@users.noreply.github.com>
2022-02-22 15:14:16 -0300

    Primeira interação com firestore
    Fazendo escrita e leitura simples de campos para o firestore

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,26 +1,28 @@
 import {app, signIn} from "../../services/firebase";
 import {signInWithPopup, GoogleAuthProvider, getAuth, signOut, signInWithEmailAndPassword} from "firebase/auth";
+import { doc, getDoc, getFirestore, setDoc } from "firebase/firestore";
 export default function Home() {
+  var user;
+  const db = getFirestore(app);
+  const auth = getAuth();
+  const provider = new GoogleAuthProvider();
 
   function Logout(){
     var result_p = document.getElementById("resposta");
-    const auth = getAuth();
-    signOut(auth).then((result)=>{
-      console.log("FOI");
+    signOut(auth).then(()=>{
       result_p.innerHTML = "Desconectado";
+      user = null;
     }).catch((error)=>{
       console.log(error);
     })
   }
   function signInGoogle(){
     var result_p = document.getElementById("resposta");
-    const provider = new GoogleAuthProvider();
-    const auth = getAuth();
 
     signInWithPopup(auth, provider)
     .then((result) => {
       // The signed-in user info.
-      const user = result.user;
+      user = result.user;
       result_p.innerHTML = user.displayName+"<br>"+user.email;
       // ...
     }).catch((error) => {
@@ -34,14 +36,49 @@ export default function Home() {
     var result_p = document.getElementById("resposta");
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
-    const auth = getAuth();
     signInWithEmailAndPassword(auth, email, password)
     .then((result)=>{
+      user = result.user;
       result_p.innerHTML = result.user.displayName+"<br>"+result.user.email;
     })
     .catch((error)=>{
       console.log(error);
       result_p.innerHTML = "Erro no login";
+    });
+  }
+  
+  function writeText(){
+    var spa = doc(db, user.uid+'/dados');
+    const dada = {
+      nome: user.displayName,
+      idade: 20
+    }
+    
+    setDoc(spa, dada, {merge: false}).then(()=>{
+      console.log("Enviado");
+    }).catch((error) =>{
+      console.log(error);
+    })
+    
+  }
+  async function readText(){
+    var spa = doc(db, user.uid+'/dados');
+    var fav = getDoc(spa).then(result =>{
+      
+    })
+    getDoc(spa).then((result)=>console.log(result.data()));
+
+    var docRef = db.collection("cities").doc("SF");
+
+    docRef.get().then((doc) => {
+        if (doc.exists) {
+            console.log("Document data:", doc.data());
+        } else {
+            // doc.data() will be undefined in this case
+            console.log("No such document!");
+        }
+    }).catch((error) => {
+        console.log("Error getting document:", error);
     });
   }
   return (
@@ -54,6 +91,8 @@ export default function Home() {
       <p id="resposta"></p>
       <button
         onClick={Logout}>Logout</button>
+      <button onClick={writeText}>Escrever</button>
+      <button onClick={readText}>Ler</button>
     </div>
 
   );
