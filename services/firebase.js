3b088585695dd21434ccc5442a2ad2fde0ade24f
Davidson <70776280+davidson-nilson@users.noreply.github.com>
2022-02-22 05:24:52 -0300

    primeiro commit

---
+++ services/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyCZpzXGwEUxGJ_nMOafsbqtqWYryKN_OdI",
+  authDomain: "teste-d7613.firebaseapp.com",
+  projectId: "teste-d7613",
+  storageBucket: "teste-d7613.appspot.com",
+  messagingSenderId: "619777012722",
+  appId: "1:619777012722:web:77af13c3b3d922e363316b",
+  measurementId: "G-D749YRL2PD"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+export default app;
